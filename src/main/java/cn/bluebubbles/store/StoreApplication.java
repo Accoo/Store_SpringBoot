6aa6a5b9506a76c8654af1837615ee26c262cb5c
Yi Bo <yibo141@qq.com>
2019-01-11 08:49:23 +0800

    创建工程

---
+++ src/main/java/cn/bluebubbles/store/StoreApplication.java
@@ -0,0 +1,14 @@
+package cn.bluebubbles.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
+
+}
+
